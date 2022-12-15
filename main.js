34c76995d6126ccd6349d1d9d47bbc3a8e6b70d1
Sirjamo1 <jam_how@hotmail.com>
2022-12-16 09:43:31 +1000

    added comments to classes/functions

--- main.js
+++ main.js
@@ -7,7 +7,9 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
         prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
     }
 };
+
 class Node {
+ //data/left/right is null unless overwritten by values
     constructor(data = null, left = null, right = null) {
         this.data = data;
         this.left = left;
@@ -16,30 +18,37 @@ class Node {
 }
 class Tree {
     constructor(arr) {
+     //sort array ascending
         arr.sort((a, b) => {
             return a - b;
         });
+        //remove duplicates
         let duplicateFreeArr = [];
         for (let i = 0; i < arr.length; i += 1) {
             if (arr[i] !== arr[i - 1]) {
                 duplicateFreeArr.push(arr[i]);
             }
         }
+        //run buildTree with duplicate free array, 0 (start) and array length - 1 (end)
         this.root = this.buildTree(
             duplicateFreeArr,
             0,
-            duplicateFreeArr.length - 1
+            duplicateFreeArr.length -1
         );
+        //console.logs the (readable) result 
         prettyPrint(this.root);
     }
-    buildTree(duplicateFreeArr, start, end) {
+    buildTree(arr, start, end) {
+     //if start > end return null (works way back up the call stack)
         if (start > end) return null;
-
+        //find middle index
         let midArr = parseInt((start + end) / 2);
-        let root = new Node(duplicateFreeArr[midArr]);
-
-        root.left = this.buildTree(duplicateFreeArr, start, midArr - 1);
-        root.right = this.buildTree(duplicateFreeArr, midArr + 1, end);
+        // assign root with arr middle index value
+        let root = new Node(arr[midArr]);
+        // rerun function for left using same arr/start but end will be arr[midArr - 1] (assigning it to current root.left)
+        root.left = this.buildTree(arr, start, midArr - 1);
+        // rerun function for right using same arr/end but start will be arr[midArr + 1] (assigning it to current root.right)
+        root.right = this.buildTree(arr, midArr + 1, end);
         return root;
     }
 }
