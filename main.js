d61ddce9a9bdc12736649a8a4c86125c2734daf5
Sirjamo1 <jam_how@hotmail.com>
2022-12-17 12:04:51 +1000

    deleteNode method working (see if i can make it more readable/cut down on code in future)

--- main.js
+++ main.js
@@ -55,31 +55,92 @@ class Tree {
         // look for node to append to
         let nodeToAppendTo = this.searchForNode(this.root, value);
         // append new node to left/right depending on values
-        if (nodeToAppendTo.data > value) return nodeToAppendTo.left = new Node(value)
-        if (nodeToAppendTo.data < value) return nodeToAppendTo.right = new Node(value)
+        if (nodeToAppendTo.data > value)
+            return (nodeToAppendTo.left = new Node(value));
+        if (nodeToAppendTo.data < value)
+            return (nodeToAppendTo.right = new Node(value));
     }
-    // search(root, key) {
-    //     if (root === null || root.data === key) return root;
-    //     if (root.data < key) 
-    //     return this.search(root.right, key);
-    //     return this.search(root.left, key);
-    // }
+    search(root, key) {
+        if (root === null || root.data === key) return root;
+        if (root.data < key) return this.search(root.right, key);
+        return this.search(root.left, key);
+    }
     searchForNode(root, key) {
-        console.log(root, key);
+        // if key already exists console.log message
         if (root.data === key) return console.log(`${key} already exists`);
-        if (root.data < key && root.right === null || root.data > key && root.left === null) return root;
+        // if the node value is smaller then key and it's right is empty or node value is larger and it's left is empty return that node
+        if (
+            (root.data < key && root.right === null) ||
+            (root.data > key && root.left === null)
+        )
+            return root;
 
         if (root.data > key)
-        // root larger search left
-        return this.searchForNode(root.left, key);
+            // root larger search left
+            return this.searchForNode(root.left, key);
         // root smaller search right
         return this.searchForNode(root.right, key);
-        
+    }
+    deleteNode(value) {
+        //check if it exists
+        let nodeToDelete = this.search(this.root, value);
+        if (nodeToDelete === null) return console.log("No such value");
+
+        console.log(nodeToDelete);
+        // find parent nod
+        let parentOfNode = this.searchForParent(this.root, value);
+        console.log(parentOfNode);
+        // if node to delete has NO CHILDREN assign it's parent left/right to null
+        if (nodeToDelete.left === null && nodeToDelete.right === null) {
+            console.log("here");
+            if (parentOfNode.left?.data === value) {
+                return (parentOfNode.left = null);
+            }
+            if (parentOfNode.right?.data === value)
+                return (parentOfNode.right = null);
+        }
+        // if node to delete has ONE CHILD
+        if (parentOfNode.right?.data === nodeToDelete.data) {
+            if (nodeToDelete.left === null)
+                return (parentOfNode.right = nodeToDelete.right);
+            if (nodeToDelete.right === null)
+                return (parentOfNode.right = nodeToDelete.left);
+        }
+        if (parentOfNode.left?.data === nodeToDelete.data) {
+            if (nodeToDelete.left === null)
+                return (parentOfNode.left = nodeToDelete.right);
+            if (nodeToDelete.right === null)
+                return (parentOfNode.left = nodeToDelete.left);
+        }
+        // if node to delete has TWO CHILDREN
+        let nodeWithMoreValue = nodeToDelete.right;
+        if (nodeWithMoreValue.left === null) {
+            nodeToDelete.data = nodeWithMoreValue.data;
+            nodeToDelete.right = nodeWithMoreValue.right;
+            return;
+        }
+        let bottomLeftOfTree = this.searchBottomLeftTree(nodeWithMoreValue);
+        nodeWithMoreValue.left = bottomLeftOfTree.right;
+        nodeToDelete.data = bottomLeftOfTree.data;
+    }
+    searchForParent(root, key) {
+        if (root.data === key) return key;
+        if (root.right?.data === key || root.left?.data === key) return root;
+        // console.log(root.data)
+        if (root.data > key) return this.searchForParent(root.left, key);
+        return this.searchForParent(root.right, key);
+    }
+    searchBottomLeftTree(root) {
+        if (!root.left?.data) return root;
+        return this.searchBottomLeftTree(root.left);
     }
 }
 
-let testArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+let testArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 8888];
 let testNode = new Tree(testArr);
 prettyPrint(testNode.root);
-testNode.insert(6345);
+testNode.insert(6);
+console.log("after addition");
+prettyPrint(testNode.root);
+testNode.deleteNode(6345);
 prettyPrint(testNode.root);
